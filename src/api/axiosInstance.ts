9b278af77f9f47e1d095e31750bd16eaa0e55a8b
valera <traberker901@gmail.com>
2022-12-01 18:26:46 +0300

    bugFix: url vercel

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const baseInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "chat-backend-v1-lze0rmscg-valeriy-radionov.vercel.app",
+  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://chat-backend-v1.vercel.app",
 })
