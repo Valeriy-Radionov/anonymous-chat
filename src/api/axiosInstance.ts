c7e94964ad0f476730c4d9fac5490816bd67fd70
valera <traberker901@gmail.com>
2022-12-01 17:20:59 +0300

    features: fix instane axios

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const baseInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "chat-backend-v1-qif680pcb-valeriy-radionov.vercel.app",
+  baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "chat-backend-v1-lze0rmscg-valeriy-radionov.vercel.app",
 })
